127ad37ce8802532eb4e3aa7d46c283fa795c76c
KrasnovDaniil1 <kd69931@gmail.com>
2024-12-10 20:44:39 +0200

    add

---
+++ src/components/SortPanel.vue
@@ -0,0 +1,101 @@
+<script setup>
+import { ref } from "vue";
+
+let sortBonds = [
+    {
+        value: "yearProfit",
+        title: "Доходность",
+    },
+    {
+        value: "expireYears",
+        title: "Осталось лет",
+    },
+    {
+        value: "COUPONPERIOD",
+        title: "Купоны раз в",
+    },
+    {
+        value: "ACCRUEDINT",
+        title: "НКД",
+    },
+    {
+        value: "expireNextCoupon",
+        title: "Купон через",
+    },
+];
+let sortListing = [
+    {
+        value: 1,
+        title: "Листинг 1",
+    },
+    {
+        value: 2,
+        title: "Листинг 2",
+    },
+    {
+        value: 3,
+        title: "Листинг 3",
+    },
+];
+let sortBy = [
+    {
+        value: "up",
+        title: "Возрастанию",
+    },
+    {
+        value: "down",
+        title: "Убыванию",
+    },
+];
+let sortSelect = ref({
+    sortBonds: "yearProfit",
+    sortListing: 1,
+    sortBy: "up",
+});
+</script>
+
+<template>
+    <nav>
+        <select v-model="sortSelect.sortBonds">
+            <option
+                v-for="(item, index) of sortBonds"
+                :key="index"
+                :value="item.value"
+            >
+                {{ item.title }}
+            </option>
+        </select>
+        <select v-model="sortSelect.sortListing">
+            <option
+                v-for="(item, index) of sortListing"
+                :key="index"
+                :value="item.value"
+            >
+                {{ item.title }}
+            </option>
+        </select>
+        <select v-model="sortSelect.sortBy">
+            <option
+                v-for="(item, index) of sortBy"
+                :key="index"
+                :value="item.value"
+            >
+                {{ item.title }}
+            </option>
+        </select>
+        {{ sortSelect }}
+    </nav>
+</template>
+
+<style lang="scss" scoped>
+nav {
+    margin-top: 18px;
+    display: flex;
+    gap: 18px;
+    select {
+        background: var(--color-4);
+        border-radius: 6px;
+        padding: 5px;
+    }
+}
+</style>
--- src/views/NotInformation.vue
+++ src/views/NotInformation.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Table from "../components/Table.vue";
+import SortPanel from "../components/SortPanel.vue";
 
 import { computed } from "vue";
 import { useStore } from "vuex";
@@ -10,6 +11,14 @@ const bonds = computed(() => store.getters.getFalseInformation);
 
 <template>
     <section>
+        <SortPanel />
         <Table :bonds="bonds" />
     </section>
 </template>
+
+<style lang="scss" scoped>
+section {
+    display: flex;
+    flex-direction: column;
+}
+</style>
